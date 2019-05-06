7991f33c775a29f1752b8c16ea4e276794a85184
jannikrouthe <Jannikrouthe@ymail.com>
2019-05-06 14:28:12 +0200

    Index layout v1

---
+++ index.php
@@ -0,0 +1,161 @@
+<html>
+    <head>
+        <title>Projekt cryp</title>
+        <script
+        src="https://code.jquery.com/jquery-3.3.1.min.js"
+        integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+        crossorigin="anonymous"></script>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+    </head>
+    <style>
+* {
+  box-sizing: border-box;
+}
+
+body {
+  font-family: Arial, Helvetica, sans-serif;
+    margin: 0px;
+}
+
+header {
+  background-color: black;
+  padding: 20px;
+  text-align: center;
+  font-size: 35px;
+  color: white;
+}
+
+nav {
+  float: left;
+  width: 10%;
+  height: 80%; 
+  background: #3D9EFF;
+  padding: 20px;
+}
+nav ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+section:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+
+footer {
+  background-color: black;
+  padding: 10px;
+  text-align: center;
+  color: white;
+position: fixed;
+    height: 7%;
+    width: 100%;
+    top: 93%;
+}
+
+#Rot13style{
+    color: red;
+}
+#Text1{
+
+}
+#Text2{
+float: right;
+        }
+#t1{
+    width: 33%;        
+        }
+#t2{
+    width: 33%;   
+    text-align: center;
+        }
+#t3{
+    width: 33%;   
+        }
+#t4{
+    width: 33%;        
+        }
+#Table1{
+         width: 90%;
+        }
+#div1{
+    
+        }
+#div2{
+    
+        }
+.Divtext{
+    width: 90%;
+    height: 400px;
+    margin: 10px;
+        }
+#b1{
+     width: 100px;
+    height: 33px;
+            
+        }
+#b2{
+    width: 100px;
+    height: 33px;
+            
+        }
+</style>
+<body>
+
+
+<header>
+  <h2>Crypt</h2>
+</header>
+
+<section>
+  <nav>
+    <ul>
+        <input type="radio" name="gender" id="Rot13style" value="Rot13"> Rot13<br>
+        <input type="radio" name="gender" value="Rsa"> RSA<br>
+        <input type="radio" name="gender" value="Aes"> AES<br>
+        <input type="radio" name="gender" value="Aes"> AES<br>
+        <input type="radio" name="gender" value="Aes"> AES<br>
+        <input type="radio" name="gender" value="Aes"> AES<br>
+        <input type="radio" name="gender" value="Aes"> AES<br>
+        <input type="radio" name="gender" value="Aes"> AES<br>
+        <input type="radio" name="gender" value="Aes"> AES<br>
+        <input type="radio" name="gender" value="Aes"> AES<br>
+        <input type="radio" name="gender" value="Aes"> AES<br>
+        <input type="radio" name="gender" value="Aes"> AE<br>
+        <input type="radio" name="gender" value="Aes"> AES<br>
+        <input type="radio" name="gender" value="Aes"> AE<br>
+        
+    </ul>
+  </nav>
+    <table id="Table1">
+        <td id="t1">
+    <div id="div1">
+        <textarea class="Divtext" id="Text1"></textarea>
+    </div>
+        </td>
+        <td id="t2">
+        <button id="b1">Decrypt</button><br><br><textarea id="t4"></textarea><br><br>
+        <button id="b2">Encrypt</button>
+            
+        </td>
+        <td id="t3">
+    <div id="div2">
+        <textarea class="Divtext" id=Text2></textarea>
+    </div>
+        </td>
+    </table>
+    
+    
+    
+  
+</section>
+
+<footer>
+  <p>Aksel & Jannik</p>
+</footer>
+
+</body>
+</html>
