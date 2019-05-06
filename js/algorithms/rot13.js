325ae82e6d960363bc280f60cc7b50836fb3a2f0
Aksel Mads <wastedpotentialtonlage@gmail.com>
2019-05-06 13:30:33 +0200

    Added Ceasar cipher encryption

---
+++ js/algorithms/rot13.js
@@ -0,0 +1,22 @@
+algorithms.push({"name": "Rot13", "Enc": rot13encrypt, "Dec": rot13decrypt})
+
+let alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "x", "y", "z"] 
+
+function rot13encrypt(plain, key) {
+	let cipherText
+	for (i=0, i<plain.length; i++) {
+		let char = plain[i]
+		let index = alphabet.indexOf(char) + key
+		if (index > 25) {
+			index -= 26
+		} else if (index < 0) {
+			index += 26
+		}
+		cipherText += alphabet[index]
+	}
+	return cipherText
+}
+
+function rot13decrypt(cipher, key) {
+	return rot13encrypt(cipher, -key)
+}
