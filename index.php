0d363f92a7bf3d73956abdeda9b109e90abde455
jannikrouthe <Jannikrouthe@ymail.com>
2019-05-09 08:13:50 +0200

    Så retelser

--- index.php
+++ index.php
@@ -1,80 +1,82 @@
 <html>
-    <head>
-        <title>Projekt cryp</title>
-        <script
-        src="https://code.jquery.com/jquery-3.3.1.min.js"
-        integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
-        crossorigin="anonymous" defer></script>
-        <script type="text/javascript" src="js/base.js" defer></script>
-        <script type="text/javascript" src="js/algorithms/rot13.js" defer></script>
-        <script type="text/javascript" src="js/algorithms/xor.js" defer></script>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" type="text/css" href="indexcss.css">
-    </head>
-    <style>
+
+<head>
+    <title>Projekt cryp</title>
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous" defer></script>
+    <script type="text/javascript" src="js/base.js" defer></script>
+    <script type="text/javascript" src="js/algorithms/rot13.js" defer></script>
+    <script type="text/javascript" src="js/algorithms/xor.js" defer></script>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="indexcss.css">
+</head>
+<style>
 
 </style>
+
 <body>
 
 
-<header>
-  <h2>Crypt</h2>
-</header>
+    <header>
+        <h2>Crypt</h2>
+    </header>
 
-<section>
-  <nav id="nav1">
-    <ul id="menu">
-        
-    </ul>
-  </nav>
-     <nav id="nav2">
-  </nav>
-    <table id="Table1">
-        <td id="t1">
-    <div id="div1">
-        <textarea class="Divtext" id="Text1" placeholder="your text"></textarea>
-    </div>
-        </td>
-        <td id="t2">
-        <button id="b1" onclick="decClick()">Decrypt</button><br><br>
-        <button id="b2" onclick="encClick()">Encrypt</button><br><br><textarea id="t4" placeholder="Key"></textarea><br><br>
-        <select id="sel3">
-        <option value="Tal">Tal 0-9</option>
-        <option value="Binary">Binary 0-1</option>
-        <option value="HRX">HEX #FF</option>
-        <option value="ASCII">ASCII</option>
-        </select><br><br>
-        <button id="b3" onclick="genClick()">Genkey</button><br>
-        <select id="sel1">
-        <option value="Tal">Tal 0-9</option>
-        <option value="Binary">Binary 0-1</option>
-        <option value="HRX">HEX #FF</option>
-        <option value="ASCII">ASCII</option>
-        </select>
-        <select id="sel2">
-        <option value="Tal">Tal 0-9</option>
-        <option value="Binary">Binary 0-1</option>
-        <option value="HRX">HEX #FF</option>
-        <option value="ASCII">ASCII</option>
-        </select>
-            
-        </td>
-        <td id="t3">
-    <div id="div2">
-        <textarea class="Divtext" id=Text2 placeholder="Not Set the text here"></textarea>
-    </div>
-        </td>
-    </table>
-    
-    
-    
-  
-</section>
+    <section>
+        <nav id="nav1">
+            <ul id="menu">
 
-<footer>
-  <p>Aksel & Jannik</p>
-</footer>
+            </ul>
+        </nav>
+        <nav id="nav2">
+            <p>Hej med dig, dette er et projekt.</p>
+        </nav>
+
+        <table id="Table1">
+            <td id="t1">
+                <div id="div1">
+                    <textarea class="Divtext" id="Text1" placeholder="your text"></textarea>
+                </div>
+            </td>
+            <td id="t2">
+                <button id="b1" onclick="decClick()">Decrypt</button><br><br>
+                <button id="b2" onclick="encClick()">Encrypt</button><br><br><textarea id="t4" placeholder="Key"></textarea><br><br>
+                <select id="sel3">
+                    <option value="Tal">Tal 0-9</option>
+                    <option value="Binary">Binary 0-1</option>
+                    <option value="HRX">HEX #FF</option>
+                    <option value="ASCII">ASCII</option>
+                </select><br><br>
+                <button id="b3" onclick="genClick()">Genkey</button><br>
+                <select id="sel1">
+                    <option value="Tal">Tal 0-9</option>
+                    <option value="Binary">Binary 0-1</option>
+                    <option value="HRX">HEX #FF</option>
+                    <option value="ASCII">ASCII</option>
+                </select>
+                <select id="sel2">
+                    <option value="Tal">Tal 0-9</option>
+                    <option value="Binary">Binary 0-1</option>
+                    <option value="HRX">HEX #FF</option>
+                    <option value="ASCII">ASCII</option>
+                </select>
+
+            </td>
+            <td id="t3">
+                <div id="div2">
+                    <textarea class="Divtext" id=Text2 placeholder="Not Set the text here"></textarea>
+                </div>
+            </td>
+        </table>
+
+
+
+
+    </section>
+
+    <footer>
+        <p>Aksel & Jannik</p>
+    </footer>
 
 </body>
+
 </html>
--- js/base.js
+++ js/base.js
@@ -1,66 +1,67 @@
 let algorithms = []
 
 function encClick() {
-	let algo = $("input.algoritmer:checked").val()
-	console.log(algo)
-	$("#Text2").val(algorithms[algo].enc($("#Text1").val(), $("#t4").val()))
+    let algo = $("input.algoritmer:checked").val()
+    console.log(algo)
+    $("#Text2").val(algorithms[algo].enc($("#Text1").val(), $("#t4").val()))
 }
 
 function decClick() {
-	let algo = $("input.algoritmer:checked").val()
-	$("#Text1").val(algorithms[algo].dec($("#Text2").val(), $("#t4").val()))
+    let algo = $("input.algoritmer:checked").val()
+    $("#Text1").val(algorithms[algo].dec($("#Text2").val(), $("#t4").val()))
 }
 
 
 
 function renderAlgorithms() {
-	console.log("rendering")
-	nav = $("#menu")
-	console.log(nav)
-	nav.empty()
-	console.log(nav.empty)
-	for (i=0; i<algorithms.length;i++) {
-		nav.append('<input type="radio" name="algorithms" class="algoritmer" value="' + i + '">' + algorithms[i].name + '<br>')
-	}
+    console.log("rendering")
+    nav = $("#menu")
+    console.log(nav)
+    nav.empty()
+    console.log(nav.empty)
+    for (i = 0; i < algorithms.length; i++) {
+        nav.append('<input type="radio" name="algorithms" class="algoritmer" value="' + i + '">' + algorithms[i].name + '<br>')
+    }
 }
 
 function bitsToASCII(bitArray) {
-	let returnString = ""
-	for (i=0; i<bitArray.length; i += 8) {
-		let letter = bitArray.slice(i, i+8)
-		let letterNum = 0
-		let cc = 128
-		for (i=0;i<letter.length;i++) {
-			if (letter[i]) {
-				letterNum += cc
-			}
-			cc /= 2
-		}
-		returnString += String.fromCharCode(letterNum)
-	}
-	return returnString
+    let returnString = ""
+    for (i = 0; i < bitArray.length; i += 8) {
+        let letter = bitArray.slice(i, i + 8)
+        let letterNum = 0
+        let cc = 128
+        for (i = 0; i < letter.length; i++) {
+            if (letter[i]) {
+                letterNum += cc
+            }
+            cc /= 2
+        }
+        returnString += String.fromCharCode(letterNum)
+    }
+    return returnString
 }
 
 function ASCIIToBits(ASCIIString) {
-	let bits = []
-	for (i=0; i<ASCIIString.length; i++) {
-		let charCode = ASCIIString[i].charCodeAt()
-		let cc = 128
-		for (p=0; p<8; p++) {
-			if (charCode > cc) {
-				charCode -= cc
-				bits.push(true)
-			} else  {
-				bits.push(false)
-			}
-			cc /= 2
-		}
-	}
-	return bits
+    let bits = []
+    for (i = 0; i < ASCIIString.length; i++) {
+        let charCode = ASCIIString[i].charCodeAt()
+        let cc = 128
+        for (p = 0; p < 8; p++) {
+            if (charCode > cc) {
+                charCode -= cc
+                bits.push(true)
+            } else {
+                bits.push(false)
+            }
+            cc /= 2
+        }
+    }
+    return bits
 }
-function Arraytobinary(TandF){
+
+function Arraytobinary(TandF) {
     let Num = ""
-    for(i = 0;i < TandF.length;i++){
+    for (i = 0; i < TandF.length; i++) {
         let bool = TandF[i]
         if (bool == true) {
             Num += "1"
@@ -68,21 +69,23 @@ function Arraytobinary(TandF){
             Num += "0"
         }
     }
-    
+
     return Num
 }
-function BinarytoArray(FadnF){
+
+function BinarytoArray(FadnF) {
     let num = []
-    for(i = 0;i < FadnF.length;i++){
+    for (i = 0; i < FadnF.length; i++) {
         let bool = FadnF[i]
         if (bool == 1)
             num.push(true)
-        else{
+        else {
             num.push(false)
         }
     }
     return num
 }
-function Binarytonumber(Numberb){
-    
+
+function Binarytonumber(Numberb) {
+
 }
